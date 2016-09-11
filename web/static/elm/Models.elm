d375bd2f63162139d3c648ec838158fe6ed33f37
Shodai Yokoyama <quantumcars@gmail.com>
2016-09-12 08:08:08 +0900

    Models: Add User

---
+++ web/static/elm/Models.elm
@@ -0,0 +1,11 @@
+module Models exposing (..)
+
+
+type alias User =
+    { avatar : String
+    }
+
+
+initialModelUser =
+    { avatar = "https://www.gravatar.com/avatar/00000000000000000000000000000000"
+    }
