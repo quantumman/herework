5c88ec333549f9628912158a944fff03ba48a442
Shodai Yokoyama <quantumcars@gmail.com>
2016-09-23 08:36:42 +0900

    Update: Add update

--- web/static/elm/Update.elm
+++ web/static/elm/Update.elm
@@ -3,5 +3,15 @@ module Update exposing (..)
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
+import Message exposing (..)
 import Models exposing (..)
 
+
+update : Msg -> Model -> ( Model, Cmd msg )
+update message model =
+    case message of
+        Fetch resource ->
+            model ! []
+
+        Open url ->
+            model ! []
