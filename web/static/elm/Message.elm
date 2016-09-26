5b597d3145ccba81b73508e1a19642cd733e556c
Shodai Yokoyama <quantumcars@gmail.com>
2016-09-27 07:31:13 +0900

    Message: Msg - Add HandleError

--- web/static/elm/Message.elm
+++ web/static/elm/Message.elm
@@ -1,5 +1,6 @@
 module Message exposing (..)
 
+import Component.Error.Message as Error exposing (..)
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
@@ -8,6 +9,7 @@ import Html.Attributes exposing (..)
 type Msg
     = Fetch Url
     | Open Url
+    | HandleError Error.Msg
 
 
 
