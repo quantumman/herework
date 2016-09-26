4a64edfe80c2e6480af0c5b9460a8eac6e2a8448
Shodai Yokoyama <quantumcars@gmail.com>
2016-09-26 18:39:24 +0900

    Error: Update - Add update

--- web/static/elm/Component/Error/Update.elm
+++ web/static/elm/Component/Error/Update.elm
@@ -2,3 +2,11 @@ module Error.Update exposing (..)
 
 import Component.Error.Message exposing (..)
 import Component.Error.Model exposing (..)
+
+
+update : Msg -> Model m -> ( Model m, Cmd Msg )
+update error model =
+    case error of
+        Http e ->
+            -- handle HTTP error
+            model ! []
