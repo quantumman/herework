6e78bb68ed018cb3652e7157389ef54e24cc7422
Shodai Yokoyama <quantumcars@gmail.com>
2016-09-27 07:20:58 +0900

    Error: Add view

--- web/static/elm/Component/Error/View.elm
+++ web/static/elm/Component/Error/View.elm
@@ -2,3 +2,16 @@ module Component.Error.View exposing (..)
 
 import Component.Error.Message exposing (..)
 import Component.Error.Model exposing (..)
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+
+
+view : Model m -> Html Msg
+view model =
+    case model.error of
+        Just message ->
+            div [] [ text message ]
+
+        Nothing ->
+            div [] []
