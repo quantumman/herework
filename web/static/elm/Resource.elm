06d48d29793682e8727460f08c18163cbb969b3f
Shodai Yokoyama <quantumcars@gmail.com>
2016-09-28 07:59:00 +0900

    Resource: Add url

--- web/static/elm/Resource.elm
+++ web/static/elm/Resource.elm
@@ -1,8 +1,26 @@
 module Resource exposing (..)
 
+import Models exposing (Url)
+
 
 type Resource
     = Messages
     | Message Int
     | Users
     | User Int
+
+
+url : Resource -> Url
+url resource =
+    case resource of
+        Messages ->
+            "/api/messages"
+
+        Message id ->
+            "/api/messages/" ++ toString id
+
+        Users ->
+            "/api/users"
+
+        User id ->
+            "/api/users/" ++ toString id
