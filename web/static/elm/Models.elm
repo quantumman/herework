8a9d24cc1a2a41899a2c7bff75baf3efa460a81b
Shodai Yokoyama <quantumcars@gmail.com>
2016-09-29 08:19:06 +0900

    Models: Add decoder/encoder for Message

--- web/static/elm/Models.elm
+++ web/static/elm/Models.elm
@@ -3,6 +3,8 @@ module Models exposing (..)
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
+import Json.Decode as Decode exposing (..)
+import Json.Encode as Encode exposing (..)
 import Router as Router exposing (..)
 
 
@@ -59,3 +61,23 @@ initialModelMessage =
     { title = "test"
     , url = "issues/1"
     }
+
+
+encodeMessage : Message -> Encode.Value
+encodeMessage model =
+    Encode.object
+        [ ( "title", Encode.string model.title )
+        , ( "url", Encode.string model.url )
+        ]
+
+
+decodeMessage : Decoder Message
+decodeMessage =
+    Decode.object2 Message
+        ("title" := Decode.string)
+        ("url" := Decode.string)
+
+
+decodeMessages : Decoder (List Message)
+decodeMessages =
+    Decode.list decodeMessage
