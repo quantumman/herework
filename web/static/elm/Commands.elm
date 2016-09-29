85c5083a68c6cc93b95dea8fce12c2498860e279
Shodai Yokoyama <quantumcars@gmail.com>
2016-09-29 09:31:09 +0900

    Commands: Add performRequest

--- web/static/elm/Commands.elm
+++ web/static/elm/Commands.elm
@@ -1,6 +1,16 @@
 module Commands exposing (..)
 
+import Component.Error.Message as Error exposing (..)
+import Http exposing (Error)
 import Json.Decode as Decode exposing (..)
 import Json.Encode as Encode exposing (..)
-import Message exposing (..)
+import Jwt exposing (..)
+import Message as App exposing (..)
 import Task exposing (Task)
+
+
+performRequest : (a -> App.Msg) -> Task Http.Error a -> Cmd App.Msg
+performRequest msg task =
+    task
+        |> Task.mapError Error.Http
+        |> Task.perform HandleError msg
