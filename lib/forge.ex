7965ce58c3d6df0c7f87868a8863d28e731dd69f
Shodai Yokoyama <quantumcars@gmail.com>
2016-10-05 07:54:58 +0900

    forge: Generate fake data for message

--- lib/forge.ex
+++ lib/forge.ex
@@ -3,4 +3,8 @@ defmodule Forge do
 
   @save_one_function &Blacksmith.Config.save/1
   @save_all_function &Blacksmith.Config.save_all/1
+
+  register :message, %Herework.Message{
+    title: Faker.Lorem.word
+  }
 end
