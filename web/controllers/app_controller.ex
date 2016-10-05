24f24f626276cc1e3fcc1620f4b5ef6ac7cfdf5c
Shodai Yokoyama <quantumcars@gmail.com>
2016-10-06 08:33:15 +0900

    app_controller: Refactor - Use path builder

--- web/controllers/app_controller.ex
+++ web/controllers/app_controller.ex
@@ -4,7 +4,7 @@ defmodule Herework.AppController do
   def index(conn, _params) do
     base = Herework.Router.Helpers.url(conn)
     app = %{
-      messages_url: base <> "/api/messages",
+      messages_url: base <> message_path(conn, :index),
       tasks_url: base <> "/api/tasks",
       activity_url: base <> "/api/activity"
     }
