97bcee6c5c2c8d675d5c7a109c00042660aebe7d
Shodai Yokoyama <quantumcars@gmail.com>
2016-10-07 08:06:03 +0900

    elm: Add Component.CommentList component

---
+++ web/static/elm/Component/CommentList.elm
@@ -0,0 +1,7 @@
+module Component.CommentList exposing (..)
+
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Message exposing (..)
