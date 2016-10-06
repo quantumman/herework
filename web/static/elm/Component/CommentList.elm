7dadbe81e90107e6322edb4e6dd46b5bbf396414
Shodai Yokoyama <quantumcars@gmail.com>
2016-10-07 08:10:07 +0900

    CommentList: Add Model

--- web/static/elm/Component/CommentList.elm
+++ web/static/elm/Component/CommentList.elm
@@ -5,3 +5,10 @@ import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Message exposing (..)
+import Models exposing (Comment)
+
+
+type alias Model m =
+    { m
+        | comments : List Comment
+    }
