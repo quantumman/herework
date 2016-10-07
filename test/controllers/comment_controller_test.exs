a943011a4c68b820e4abd3dfc6e671d178305242
Shodai Yokoyama <quantumcars@gmail.com>
2016-10-07 22:11:14 +0900

    comment_controller: Fetch nested resource properly

--- test/controllers/comment_controller_test.exs
+++ test/controllers/comment_controller_test.exs
@@ -7,13 +7,19 @@ defmodule Herework.CommentControllerTest do
   @valid_attrs %{body: "some content"}
   @invalid_attrs %{}
 
-  setup %{conn: conn} do
+  defp build_models() do
     {:ok, message} = Forge.saved_message
     {:ok, creator} = Forge.saved_user
     {:ok, comment} =
       Ecto.build_assoc(creator, :comments, Forge.comment)
       |> (&Ecto.build_assoc(message, :comments, &1)).()
       |> Herework.Repo.insert
+    { message, comment }
+  end
+
+  setup %{conn: conn} do
+    build_models
+    { message, comment } = build_models
 
     {:ok,
      conn: put_req_header(conn, "accept", "application/json"),
@@ -22,8 +28,8 @@ defmodule Herework.CommentControllerTest do
     }
   end
 
-  test "lists all entries on index", %{conn: conn, comment: comment} do
-    conn = get conn, message_comment_path(conn, :index, comment.message_id)
+  test "lists all entries on index", %{conn: conn, message: message, comment: comment} do
+    conn = get conn, message_comment_path(conn, :index, message.id)
     comment = Comment
       |> Herework.Repo.get_by(id: comment.id)
       |> Herework.Repo.preload(:creator)
@@ -71,14 +77,21 @@ defmodule Herework.CommentControllerTest do
     end
   end
 
+  test "renders page not found when message_id are nonexistent", %{conn: conn, comment: comment } do
+    assert_error_sent 404, fn ->
+      get conn, message_comment_path(conn, :show, 1, comment)
+    end
+  end
+
   test "creates and renders resource when data is valid", %{conn: conn, message: message} do
     conn = post conn, message_comment_path(conn, :create, message.id), comment: @valid_attrs
     assert json_response(conn, 201)["id"]
+    assert json_response(conn, 201)["message_id"] == message.id
     assert Repo.get_by(Comment, @valid_attrs)
   end
 
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, message_comment_path(conn, :create, -1), comment: @invalid_attrs
+  test "does not create resource and renders errors when data is invalid", %{conn: conn, message: message} do
+    conn = post conn, message_comment_path(conn, :create, message.id), comment: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
@@ -88,15 +101,21 @@ defmodule Herework.CommentControllerTest do
     assert Repo.get_by(Comment, @valid_attrs)
   end
 
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    comment = Repo.insert! %Comment{}
-    conn = put conn, message_comment_path(conn, :update, -1, comment), comment: @invalid_attrs
+  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn, message: message} do
+    comment =
+      message
+      |> Ecto.build_assoc(:comments, %Comment{})
+      |> Repo.insert!
+    conn = put conn, message_comment_path(conn, :update, message.id, comment), comment: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
-  test "deletes chosen resource", %{conn: conn} do
-    comment = Repo.insert! %Comment{}
-    conn = delete conn, message_comment_path(conn, :delete, 1, comment)
+  test "deletes chosen resource", %{conn: conn, message: message} do
+    comment =
+      message
+      |> Ecto.build_assoc(:comments, %Comment{})
+      |> Repo.insert!
+    conn = delete conn, message_comment_path(conn, :delete, message.id, comment)
     assert response(conn, 204)
     refute Repo.get(Comment, comment.id)
   end
