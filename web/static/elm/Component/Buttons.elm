d891f3b29708621972cf0653092361bde3d66cc9
Shodai Yokoyama <quantumcars@gmail.com>
2016-10-14 08:12:14 +0900

    Buttons: Add basic button function

--- web/static/elm/Component/Buttons.elm
+++ web/static/elm/Component/Buttons.elm
@@ -1,8 +1,18 @@
 module Component.Buttons exposing (..)
 
+import Aui.Buttons as Aui exposing (..)
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Message exposing (..)
 
+
+type alias ButtonType =
+    Config Msg -> Config Msg
+
+
+button : ButtonType -> Msg -> List (Html Msg) -> Html Msg
+button config message content =
+    Aui.button (baseConfig |> config |> withAction message)
+        content
