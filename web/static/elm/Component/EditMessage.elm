4db61e7802a4d5e12512dbfcb85f728c64a5c88a
Shodai Yokoyama <quantumcars@gmail.com>
2016-10-19 08:59:36 +0900

    Component: Add EditMessage

--- web/static/elm/Component/EditMessage.elm
+++ web/static/elm/Component/EditMessage.elm
@@ -1,8 +1,40 @@
-module Component.EditMessage exposing(..)
+module Component.EditMessage exposing (..)
 
+import Component.Form exposing (bind, bindCheck)
+import Component.Buttons as Buttons exposing (..)
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Message exposing (..)
 import Models exposing (..)
+
+
+-- MODEL
+
+
+type alias Model m =
+    { m
+        | newMessage : Message
+    }
+
+
+
+-- VIEW
+
+
+view : Model m -> Html Msg
+view model =
+    div []
+        [ Html.form []
+            [ div []
+                [ textarea [ rows 1 ] []
+                ]
+            , div []
+                [ textarea [] []
+                ]
+            ]
+        , Buttons.button primary
+            (AddMessage model.newMessage)
+            [ text "Add" ]
+        ]
