0d84565b6b6cec3bafc1690473f21ed12f05b17c
Shodai Yokoyama <quantumcars@gmail.com>
2016-10-19 08:50:45 +0900

    elm: Add EditMessage

---
+++ web/static/elm/Component/EditMessage.elm
@@ -0,0 +1,8 @@
+module Component.EditMessage exposing(..)
+
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Message exposing (..)
+import Models exposing (..)
