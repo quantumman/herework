c7bb80f8486811dcd097ba0b2cb18c12fd9f244f
Shodai Yokoyama <quantumcars@gmail.com>
2016-10-20 06:52:58 +0900

    Form: Add Binder

--- web/static/elm/Component/Form.elm
+++ web/static/elm/Component/Form.elm
@@ -2,3 +2,10 @@ module Component.Form exposing (..)
 
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+
+
+-- UPDATE
+
+
+type Binder model
+    = Binder (model -> String -> model)
