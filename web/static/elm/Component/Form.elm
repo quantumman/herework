78ce8c69c5c81ee3cc7328d1e737b92b1b3a81e8
Shodai Yokoyama <quantumcars@gmail.com>
2016-10-20 07:36:07 +0900

    Form: Add a helper for binding text box

--- web/static/elm/Component/Form.elm
+++ web/static/elm/Component/Form.elm
@@ -1,7 +1,21 @@
 module Component.Form exposing (..)
 
+import Html exposing (..)
+import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Json.Decode as Json exposing (..)
+
+
+-- Data binding helper
+
+
+bind : Binder model -> (Msg model -> msg) -> Attribute msg
+bind binder f =
+    Json.map (Bind binder) targetValue
+        |> Json.map f
+        |> on "input"
+
 
 
 -- UPDATE
