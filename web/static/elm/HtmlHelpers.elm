c8beded7debe8ede2cda80240fae561c8254eef7
Shodai Yokoyama <quantumcars@gmail.com>
2016-10-21 07:32:14 +0900

    elm: Add HtmlHelpers

---
+++ web/static/elm/HtmlHelpers.elm
@@ -0,0 +1,7 @@
+module HtmlHelpers exposing (..)
+
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Message exposing (..)
+import Models exposing (..)
