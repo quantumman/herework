1de40b97ca9416255e60c1c5a9def0285956f8cc
Shodai Yokoyama <quantumcars@gmail.com>
2016-11-02 07:34:35 +0900

    Fix test for user

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,7 +3,7 @@ defmodule Herework.UserTest do
 
   alias Herework.User
 
-  @valid_attrs %{email: "some content", password: "some content"}
+  @valid_attrs %{email: "email@example.com", password: "some content"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
