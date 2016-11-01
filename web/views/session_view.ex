bcd27c367dded8d0bfe430c08c7a8ee938f506d7
Shodai Yokoyama <quantumcars@gmail.com>
2016-11-01 22:17:45 +0900

    session_view: Remove unused responses

--- web/views/session_view.ex
+++ web/views/session_view.ex
@@ -1,14 +1,3 @@
 defmodule Herework.SessionView do
   use Herework.Web, :view
-
-  def render("show.json", %{jwt: jwt, user: user}) do
-    %{
-      jwt: jwt,
-      user: user
-    }
-  end
-
-  def render("error.json", _) do
-    %{error: "Invalid email or password"}
-  end
 end
