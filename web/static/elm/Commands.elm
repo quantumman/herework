be5a640a67720930201e25561a741d043a64fdfe
Shodai Yokoyama <quantumcars@gmail.com>
2016-11-01 09:16:43 +0900

    Commands: Send http request without JWT

--- web/static/elm/Commands.elm
+++ web/static/elm/Commands.elm
@@ -40,34 +40,34 @@ performRequest msg task =
 
 get : Decoder a -> Url -> (a -> App.Msg) -> Cmd App.Msg
 get decoder resource msg =
-    Jwt.get "1238a" decoder resource
+    send "GET" decoder resource Http.empty
         |> performRequest msg
 
 
 post : Decoder a -> Url -> Encode.Value -> (a -> App.Msg) -> Cmd App.Msg
 post decoder resource payload msg =
     Http.string (Encode.encode 0 payload)
-        |> Jwt.post "1238a" decoder resource
+        |> send "POST" decoder resource
         |> performRequest msg
 
 
 put : Decoder a -> Url -> Encode.Value -> (a -> App.Msg) -> Cmd App.Msg
 put decoder resource payload msg =
     Http.string (Encode.encode 0 payload)
-        |> Jwt.send "PUT" "1238a" decoder resource
+        |> send "PUT" decoder resource
         |> performRequest msg
 
 
 patch : Decoder a -> Url -> Encode.Value -> (a -> App.Msg) -> Cmd App.Msg
 patch decoder resource payload msg =
     Http.string (Encode.encode 0 payload)
-        |> Jwt.send "PATCH" "1238a" decoder resource
+        |> send "PATCH" decoder resource
         |> performRequest msg
 
 
 delete : Url -> (() -> App.Msg) -> Cmd App.Msg
 delete resource msg =
-    Jwt.send "DELETE" "1238a" (Decode.null ()) resource Http.empty
+    send "DELETE" (Decode.null ()) resource Http.empty
         |> performRequest msg
 
 
