51df7b1569cfb1383e05f20d5f3935b67a0484bd
Shodai Yokoyama <quantumcars@gmail.com>
2016-11-08 07:57:42 +0900

    Error: Print http error

--- web/static/elm/Component/Error/Update.elm
+++ web/static/elm/Component/Error/Update.elm
@@ -2,6 +2,7 @@ module Component.Error.Update exposing (..)
 
 import Component.Error.Message exposing (..)
 import Component.Error.Model exposing (..)
+import Http as Http exposing (..)
 
 
 update : Msg -> Model m -> ( Model m, Cmd Msg )
@@ -9,4 +10,20 @@ update error model =
     case error of
         Http e ->
             -- handle HTTP error
-            model ! []
+            { model | error = Just (httpErrorAsString e) } ! []
+
+
+httpErrorAsString : Http.Error -> String
+httpErrorAsString e =
+    case e of
+        Http.Timeout ->
+            "Timeout"
+
+        Http.NetworkError ->
+            "Network has troubles. Try it later"
+
+        Http.UnexpectedPayload msg ->
+            msg
+
+        Http.BadResponse code msg ->
+            msg
