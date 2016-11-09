7a52a89e4c7c2749b1d6cad70282147609bcb94a
Shodai Yokoyama <quantumcars@gmail.com>
2016-11-10 08:22:31 +0900

    Callout: Add triangle of callout

--- web/static/elm/Component/Callout.elm
+++ web/static/elm/Component/Callout.elm
@@ -6,3 +6,36 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Style exposing (..)
 
+
+triangle : String -> Html msg
+triangle color =
+    let
+        outerHeight =
+            (px 6)
+
+        innerHeight =
+            (px 4)
+
+        outerTriangle =
+            style
+                [ ( "border-top", outerHeight ++ " solid transparent" )
+                , ( "border-right", outerHeight ++ " solid " ++ color )
+                , ( "border-bottom", outerHeight ++ " solid transparent" )
+                , ( "border-left", outerHeight ++ " solid transparent" )
+                , Style.width "0"
+                , Style.height "0"
+                , marginTop (px 6)
+                ]
+
+        innerTriangle =
+            style
+                [ border (innerHeight ++ " solid")
+                , borderColor "transparent #f5f5f5 transparent transparent"
+                , marginTop (px -10)
+                , marginLeft (px 1)
+                ]
+    in
+        div []
+            [ div [ outerTriangle ] []
+            , div [ innerTriangle ] []
+            ]
