44cd5a90e3370f18cd22ef6cca7046f3b8da7156
Shodai Yokoyama <quantumcars@gmail.com>
2016-11-10 08:44:08 +0900

    CommentList: Add body

--- web/static/elm/Component/CommentList.elm
+++ web/static/elm/Component/CommentList.elm
@@ -3,12 +3,13 @@ module Component.CommentList exposing (..)
 import Aui.Avatars exposing (..)
 import Component.DateTime as DateTime exposing (view, Model)
 import Component.Layout exposing (..)
+import Date exposing (Date)
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Message exposing (..)
-import Models exposing (Comment, Message)
+import Models exposing (Comment, Message, User)
 import Style exposing (..)
 
 
@@ -77,6 +78,48 @@ comment dateTime model =
 creator : Comment -> Html Msg
 creator model =
     avatar config model.creator.avatar
+
+
+body : DateTime.Model -> { m | body : String, created_at : Date, creator : User } -> Html Msg
+body dateTime model =
+    let
+        padding =
+            style
+                [ paddingLeft (px 5)
+                , paddingRight (px 5)
+                , paddingTop (px 7)
+                , paddingBottom (px 7)
+                ]
+
+        bodyStyle =
+            style
+                [ minHeight (Style.em 4)
+                ]
+
+        footerStyle =
+            style
+                [ ( "border-top", "1px solid #ddd" )
+                , fontSize (Style.em 0.8)
+                , color "#777"
+                ]
+
+        floatRight =
+            style [ float right' ]
+
+        clearRight =
+            style [ clear right' ]
+    in
+        div []
+            [ div [ padding, bodyStyle ] [ text model.body ]
+            , div [ footerStyle ]
+                [ div [ padding, floatRight ]
+                    [ text model.creator.name
+                    , text " POSTED ON "
+                    , Html.map Now <| DateTime.view model.created_at dateTime
+                    ]
+                , div [ clearRight ] []
+                ]
+            ]
 
 
 callout : Html Msg
