fd18491784e93d79c9f32764bd84bc059d9cfb19
Shodai Yokoyama <quantumcars@gmail.com>
2016-11-24 14:09:16 +0900

    DOM: Add selectionStart decoder

--- web/static/elm/Component/Infrastructures/DOM.elm
+++ web/static/elm/Component/Infrastructures/DOM.elm
@@ -1,3 +1,9 @@
 module Component.Infrastructures.DOM exposing (..)
 
+import DOM exposing (..)
 import Json.Decode as Decode exposing (..)
+
+
+selectionStart : Decoder Int
+selectionStart =
+    field "selectionStart" Decode.int
