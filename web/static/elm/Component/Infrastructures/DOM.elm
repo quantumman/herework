f55705b54a9f1d4a5a34802fc158f2b2b1abdba6
Shodai Yokoyama <quantumcars@gmail.com>
2016-11-24 14:12:08 +0900

    DOM: Add a decoder for selection range

--- web/static/elm/Component/Infrastructures/DOM.elm
+++ web/static/elm/Component/Infrastructures/DOM.elm
@@ -12,3 +12,20 @@ selectionStart =
 selectionEnd : Decoder Int
 selectionEnd =
     field "selectionEnd" Decode.int
+
+
+type alias SelectionRange =
+    { start : Int
+    , end : Int
+    }
+
+
+selectionRange : (SelectionRange -> msg) -> Decoder msg
+selectionRange fmsg =
+    let
+        range =
+            Decode.map2 SelectionRange
+                selectionStart
+                selectionEnd
+    in
+        map fmsg (target range)
