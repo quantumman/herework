e64bb9cd5889a583ef9e3d678c1d7595876a6379
Shodai Yokoyama <quantumcars@gmail.com>
2016-12-08 08:22:26 +0900

    Attribute: Add level*

--- web/static/elm/Component/UI/Attribute.elm
+++ web/static/elm/Component/UI/Attribute.elm
@@ -8,3 +8,23 @@ import Html.Events exposing (..)
 active : Attribute msg
 active =
     class "is-active"
+
+
+level : Attribute msg
+level =
+    class "level"
+
+
+levelLeft : Attribute msg
+levelLeft =
+    class "level-left"
+
+
+levelRight : Attribute msg
+levelRight =
+    class "level-right"
+
+
+levelItem : Attribute msg
+levelItem =
+    class "level-item"
