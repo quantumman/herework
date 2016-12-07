3df387e4e0d96088bd7f2d7629305ea3b8ebcc5d
Shodai Yokoyama <quantumcars@gmail.com>
2016-12-08 08:12:22 +0900

    Attribute: Add active

--- web/static/elm/Component/UI/Attribute.elm
+++ web/static/elm/Component/UI/Attribute.elm
@@ -3,3 +3,8 @@ module Component.UI.Attribute exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+
+
+active : Attribute msg
+active =
+    class "is-active"
