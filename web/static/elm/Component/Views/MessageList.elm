e2c316bb5ecab533ac4477b1fd23739d2f82d2fa
Shodai Yokoyama <quantumcars@gmail.com>
2016-12-09 08:51:32 +0900

    MessageList: Show messages

--- web/static/elm/Component/Views/MessageList.elm
+++ web/static/elm/Component/Views/MessageList.elm
@@ -1,8 +1,11 @@
 module Component.Views.MessageList exposing (..)
 
+import Component.UI.Attribute as Attribute exposing (..)
+import Component.UI.MediaObject as MediaObject exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Models.Message exposing (..)
 
 
 type alias Model m =
@@ -11,6 +14,22 @@ type alias Model m =
     }
 
 
-view : Html msg
-view =
-    div [] []
+view : Model m -> Html msg
+view { messages } =
+    let
+        render message =
+            MediaObject.media
+                [ MediaObject.left
+                    [ p [ Attribute.image, class "is-32x32" ]
+                        [ img [ src message.creator.avatar ] [] ]
+                    ]
+                , MediaObject.content
+                    [ div [ Attribute.content ]
+                        [ p []
+                            [ text message.title ]
+                        ]
+                    ]
+                ]
+    in
+        div []
+            (List.map render messages)
