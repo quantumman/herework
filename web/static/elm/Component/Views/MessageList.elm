f44d233579bd1a40336def791afcfd472396b2d8
Shodai Yokoyama <quantumcars@gmail.com>
2016-12-14 22:37:40 +0900

    MessageList: Add selection effect

--- web/static/elm/Component/Views/MessageList.elm
+++ web/static/elm/Component/Views/MessageList.elm
@@ -6,6 +6,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import HtmlHelpers exposing (..)
+import InlineHover exposing (..)
 import Message exposing (..)
 import Models.Message exposing (..)
 import Router exposing (Route(..))
@@ -14,6 +15,7 @@ import Router exposing (Route(..))
 type alias Model m =
     { m
         | messages : List Message
+        , selectedMessage : Maybe Message
     }
 
 
@@ -29,21 +31,74 @@ avatarStyle =
     ]
 
 
+selected : Message -> Model m -> List ( String, String )
+selected message { selectedMessage } =
+    let
+        makeStyle m =
+            if m.id == message.id then
+                [ ( "background-color", "#00d1b2" )
+                , ( "color", "white" )
+                ]
+            else
+                []
+    in
+        selectedMessage
+            |> Maybe.map makeStyle
+            |> Maybe.withDefault []
+
+
+hoverStyle : List ( String, String )
+hoverStyle =
+    [ ( "color", "#00d1b2" )
+    , ( "background-color", "whitesmoke" )
+    ]
+
+
+tweakSpace : List ( String, String )
+tweakSpace =
+    [ ( "margin-top", "0" )
+    , ( "padding-bottom", "10px" )
+    ]
+
+
+mediaStyle : List ( String, String )
+mediaStyle =
+    [ ( "color", "#4a4a4a" ) ]
+
+
+contentStyle : List ( String, String )
+contentStyle =
+    [ ( "color", "inherit" ) ]
+
+
+clickable : List ( String, String )
+clickable =
+    [ ( "cursor", "pointer" ) ]
+
+
 
 -- VIEW
 
 
 view : Model m -> Html Msg
-view { messages } =
+view model =
     let
         render message =
-            MediaObject.media []
+            hover_ message
+                model
+                MediaObject.media
+                [ navigateTo (MessageDetail message.id)
+                , style clickable
+                , style tweakSpace
+                , style mediaStyle
+                , style (selected message model)
+                ]
                 [ MediaObject.left
                     [ p [ Attribute.image, class "is-32x32" ]
                         [ img [ style avatarStyle, src message.creator.avatar ] [] ]
                     ]
-                , MediaObject.content [ style [ ( "cursor", "pointer" ) ], navigateTo (MessageDetail message.id) ]
-                    [ div [ Attribute.content ]
+                , MediaObject.content []
+                    [ div [ Attribute.content, style contentStyle ]
                         [ p []
                             [ text message.title ]
                         ]
@@ -51,4 +106,24 @@ view { messages } =
                 ]
     in
         div []
-            (List.map render messages)
+            (List.map render model.messages)
+
+
+hover_ :
+    Message
+    -> Model m
+    -> (List (Attribute Msg) -> List (Media Msg) -> Html Msg)
+    -> List (Attribute Msg)
+    -> List (Media Msg)
+    -> Html Msg
+hover_ message { selectedMessage } tag attrs children =
+    let
+        hoverOrNot m =
+            if message.id == m.id then
+                tag attrs children
+            else
+                hover hoverStyle (\a _ -> tag a children) attrs []
+    in
+        selectedMessage
+            |> Maybe.map hoverOrNot
+            |> Maybe.withDefault (tag attrs children)
