975f9e81523ea367af13223d327d767b88f17337
Shodai Yokoyama <quantumcars@gmail.com>
2016-12-24 13:10:58 +0900

    Messages: Add Edit

---
+++ web/static/elm/Component/Views/Messages/Edit.elm
@@ -0,0 +1,16 @@
+module Component.Views.Messages.Edit exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Message exposing (..)
+import Models exposing (..)
+import Router as Router exposing (..)
+
+
+-- VIEW
+
+
+view : Html Msg
+view =
+    div [] []
