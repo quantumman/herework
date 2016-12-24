307838e4913c8e89d5203c4d3060cfb8d152a387
Shodai Yokoyama <quantumcars@gmail.com>
2016-12-24 13:17:23 +0900

    Messages: Render view for EditMessage

--- web/static/elm/Component/Views/Messages.elm
+++ web/static/elm/Component/Views/Messages.elm
@@ -1,5 +1,6 @@
 module Component.Views.Messages exposing (..)
 
+import Component.Views.Messages.Edit as Edit exposing (view)
 import Component.Views.Messages.Detail as Detail exposing (view)
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -17,6 +18,9 @@ view route model =
     case route of
         MessageDetail id ->
             Detail.view model
+
+        Router.EditMessage id ->
+            Edit.view model
 
         _ ->
             div [] []
