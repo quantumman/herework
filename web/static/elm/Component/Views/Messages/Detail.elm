5cc39524e821c46781e3894967382a71e057ad28
Shodai Yokoyama <quantumcars@gmail.com>
2016-12-24 12:53:39 +0900

    Messages: Add Detail

---
+++ web/static/elm/Component/Views/Messages/Detail.elm
@@ -0,0 +1,15 @@
+module Component.Views.Messages.Detail exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Message exposing (..)
+import Models exposing (..)
+
+
+-- VIEW
+
+
+view : Html Msg
+view =
+    div [] []
