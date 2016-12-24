fe6660439fb14498d2b38b0a47d4596d124a165b
Shodai Yokoyama <quantumcars@gmail.com>
2016-12-24 13:08:59 +0900

    Messages: Render view for MessageDetail

--- web/static/elm/Component/Views/Messages.elm
+++ web/static/elm/Component/Views/Messages.elm
@@ -1,16 +1,22 @@
 module Component.Views.Messages exposing (..)
 
+import Component.Views.Messages.Detail as Detail exposing (view)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Message exposing (..)
-import Models exposing (..)
+import Models as App exposing (..)
 import Router as Router exposing (..)
 
 
 -- VIEW
 
 
-view : Route -> Html Msg
-view route =
-    div [] []
+view : Route -> App.Model -> Html Msg
+view route model =
+    case route of
+        MessageDetail id ->
+            Detail.view model
+
+        _ ->
+            div [] []
