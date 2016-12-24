04439c8b86359ba4ceaabb1c6a14e8f8dd0b101b
Shodai Yokoyama <quantumcars@gmail.com>
2016-12-24 12:47:49 +0900

    Component: Messages - Add Layout

---
+++ web/static/elm/Component/Views/Messages/Layout.elm
@@ -0,0 +1,12 @@
+module Component.Views.Messages.Layout exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Message exposing (..)
+import Models exposing (..)
+
+
+view : Html Msg
+view =
+    div [] []
