6d0b904a6c4a5a3d99cb2df65441379bd621c77c
Shodai Yokoyama <quantumcars@gmail.com>
2016-12-24 12:51:41 +0900

    Messages: Add a layout for detail page

--- web/static/elm/Component/Views/Messages/Layout.elm
+++ web/static/elm/Component/Views/Messages/Layout.elm
@@ -1,5 +1,7 @@
 module Component.Views.Messages.Layout exposing (..)
 
+import Component.UI.Attribute as Attribute exposing (..)
+import Date exposing (Date)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -7,6 +9,59 @@ import Message exposing (..)
 import Models exposing (..)
 
 
-view : Html Msg
-view =
-    div [] []
+-- STYLE
+
+
+removeSpace : List ( String, String )
+removeSpace =
+    [ ( "margin-bottom", "0" )
+    ]
+
+
+separator : List ( String, String )
+separator =
+    [ ( "margin-top", "10px" )
+    ]
+
+
+
+-- VIEW
+
+
+type alias Title =
+    List (Html Msg)
+
+
+type alias Body =
+    List (Html Msg)
+
+
+type alias CreatedAt =
+    List (Html Msg)
+
+
+type alias Creator =
+    List (Html Msg)
+
+
+type alias Actions =
+    List (Html Msg)
+
+
+layout : Title -> Body -> CreatedAt -> Creator -> Actions -> Html Msg
+layout title body createdAt avatar actions =
+    div [ Attribute.content ]
+        [ h1 [ Attribute.title 2 ] title
+        , div [ subtitle 6, style removeSpace ]
+            [ p [ level ]
+                [ div [ levelLeft ] []
+                , div [ levelRight ]
+                    [ div [ levelItem ] createdAt
+                    , div [ levelItem ] avatar
+                    , div [ levelItem ] actions
+                    ]
+                ]
+            ]
+        , hr [ style separator ] []
+        , p [] body
+        ]
