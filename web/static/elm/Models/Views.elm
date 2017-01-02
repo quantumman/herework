f2bfb8fbdc7330770999ed80727c9915e785c376
Shodai Yokoyama <quantumcars@gmail.com>
2017-01-02 21:36:19 +0900

    Models: Views - Add MessagesView

--- web/static/elm/Models/Views.elm
+++ web/static/elm/Models/Views.elm
@@ -1,10 +1,37 @@
 module Models.Views exposing (..)
 
+import Component.Views.Messages.Editor as Editor exposing (..)
+import Models.Message as Message exposing (Message, initialModel)
+import Models.Comment as Comment exposing (Comment)
+
 
 type alias Model =
-    {}
+    { messages : MessagesView
+    }
 
 
 initialModel : Model
 initialModel =
-    {}
+    { messages = initialMessageView
+    }
+
+
+type alias MessagesView =
+    { list : List Message
+    , detail : Message
+    , edit : Message
+    , new : Message
+    , comments : List Comment
+    , editor : Editor.Model
+    }
+
+
+initialMessageView : MessagesView
+initialMessageView =
+    { list = []
+    , detail = Message.initialModel
+    , edit = Message.initialModel
+    , new = Message.initialModel
+    , comments = []
+    , editor = Editor.initialModel
+    }
