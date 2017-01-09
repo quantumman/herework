91d9efae1fbcfa243e35e51d7918a5bc404fbb45
Shodai Yokoyama <quantumcars@gmail.com>
2017-01-09 19:12:55 +0900

    Message: View - Add Avatar

---
+++ web/static/elm/Message/View/Avatar.elm
@@ -0,0 +1,26 @@
+module Message.View.Avatar exposing (..)
+
+import Component.UI.Attribute as Attribute exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Models.User exposing (User)
+
+
+avatar : Int -> User -> Html msg
+avatar size creator =
+    let
+        size_ =
+            toString size
+
+        figureSize =
+            "is-" ++ size_ ++ "x" ++ size_
+
+        imgStyle =
+            [ ( "border-radius", "50%" )
+            , ( "width", size_ ++ "px" )
+            , ( "height", size_ ++ "px" )
+            ]
+    in
+        figure [ levelItem, image, class figureSize ]
+            [ img [ src creator.avatar, style imgStyle ] [] ]
