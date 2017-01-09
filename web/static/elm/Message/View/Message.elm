e3701d4af1d8bc43334ec15ef098b1a23a21e8c4
Shodai Yokoyama <quantumcars@gmail.com>
2017-01-09 20:47:01 +0900

    Message: Message - Show edit button

--- web/static/elm/Message/View/Message.elm
+++ web/static/elm/Message/View/Message.elm
@@ -2,6 +2,7 @@ module Message.View.Message exposing (..)
 
 import Component.Infrastructures.DateTime as DateTime exposing (Model)
 import Component.UI.Attribute as Attribute exposing (..)
+import Component.UI.Buttons as Buttons exposing (..)
 import Date exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -9,6 +10,8 @@ import Html.Events exposing (..)
 import Message as App exposing (Msg)
 import Message.Model as Message exposing (..)
 import Message.View.Avatar as Message exposing (..)
+import Models.Message exposing (Message)
+import Router as Router exposing (Route(..))
 
 
 -- STYLE
@@ -42,6 +45,10 @@ view now { message, isEditing } =
                 , div [ levelRight ]
                     [ div [ levelItem ] [ createdAt message.created_at now ]
                     , div [ levelItem ] [ text message.creator.name ]
+                    , if isEditing then
+                        div [] []
+                      else
+                        editButton message
                     ]
                 ]
             ]
@@ -53,3 +60,22 @@ view now { message, isEditing } =
 createdAt : Date -> DateTime.Model -> Html Msg
 createdAt date model =
     Html.map App.Now <| DateTime.view date model
+
+
+editButton : Message -> Html App.Msg
+editButton message =
+    let
+        style =
+            Buttons.default |> Buttons.small |> outlined |> primary
+
+        navigation =
+            Router.Edit message.id
+                |> Router.Messages
+                |> App.NavigateTo
+    in
+        Buttons.button style
+            navigation
+            [ span [ class "icon is-small" ]
+                [ i [ class "fa fa-pencil" ] [] ]
+            , span [] [ text "EDIT" ]
+            ]
