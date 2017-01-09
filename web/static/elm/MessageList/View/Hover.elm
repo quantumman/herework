7f00b71e2148203799ac827f9913b928ee3b653f
Shodai Yokoyama <quantumcars@gmail.com>
2017-01-09 18:01:08 +0900

    MessageList: Add Hover

---
+++ web/static/elm/MessageList/View/Hover.elm
@@ -0,0 +1,36 @@
+module MessageList.View.Hover exposing (..)
+
+import Component.UI.Attribute as Attribute exposing (..)
+import Component.UI.MediaObject as MediaObject exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import InlineHover as InlineHover exposing (..)
+import Message as App exposing (Msg)
+import MessageList.Model as MessageList exposing (Model)
+import Models.Message exposing (Message)
+
+
+hoverStyle : List ( String, String )
+hoverStyle =
+    [ ( "color", "#00d1b2" )
+    , ( "background-color", "whitesmoke" )
+    ]
+
+
+hover :
+    MessageList.Model
+    -> Message
+    -> (List (Attribute Msg) -> List (Media Msg) -> Html Msg)
+    -> List (Attribute Msg)
+    -> List (Media Msg)
+    -> Html Msg
+hover model message tag attrs children =
+    let
+        hoverOrNot m =
+            if message.id == m.id then
+                tag attrs children
+            else
+                InlineHover.hover hoverStyle (\a _ -> tag a children) attrs []
+    in
+        hoverOrNot model.message
