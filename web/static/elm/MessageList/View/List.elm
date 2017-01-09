c06e04c4f8d863eeff1ceb259314e938ff6a35e8
Shodai Yokoyama <quantumcars@gmail.com>
2017-01-09 18:08:41 +0900

    MessageList: Render message list

--- web/static/elm/MessageList/View/List.elm
+++ web/static/elm/MessageList/View/List.elm
@@ -1,12 +1,93 @@
 module MessageList.View.List exposing (..)
 
+import Component.UI.Attribute as Attribute exposing (..)
+import Component.UI.MediaObject as MediaObject exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import HtmlHelpers exposing (..)
 import Message as App exposing (Msg)
 import MessageList.Model as MessageList exposing (..)
+import MessageList.View.Hover as MessageList exposing (..)
+import Models.Message as Message exposing (Message)
+import Router as Router exposing (Route(..))
 
 
-view : Model -> Html App.Msg
+-- STYLE
+
+
+avatarStyle : List ( String, String )
+avatarStyle =
+    [ ( "border-radius", "50%" )
+    , ( "width", "32px" )
+    , ( "height", "32px" )
+    ]
+
+
+selected : Message -> MessageList.Model -> List ( String, String )
+selected message model =
+    let
+        makeStyle m =
+            if m.id == message.id then
+                [ ( "background-color", "#00d1b2" )
+                , ( "color", "white" )
+                ]
+            else
+                []
+    in
+        makeStyle model.message
+
+
+tweakSpace : List ( String, String )
+tweakSpace =
+    [ ( "margin-top", "0" )
+    , ( "padding-bottom", "10px" )
+    ]
+
+
+mediaStyle : List ( String, String )
+mediaStyle =
+    [ ( "color", "#4a4a4a" ) ]
+
+
+contentStyle : List ( String, String )
+contentStyle =
+    [ ( "color", "inherit" ) ]
+
+
+clickable : List ( String, String )
+clickable =
+    [ ( "cursor", "pointer" ) ]
+
+
+
+-- VIEW
+
+
+view : MessageList.Model -> Html App.Msg
 view model =
-    div [] []
+    let
+        render message =
+            hover model
+                message
+                MediaObject.media
+                [ navigateTo (Messages <| Router.Show message.id)
+                , style clickable
+                , style tweakSpace
+                , style mediaStyle
+                , style (selected message model)
+                ]
+                [ MediaObject.left
+                    [ p [ Attribute.image, class "is-32x32" ]
+                        [ img [ style avatarStyle, src message.creator.avatar ] [] ]
+                    ]
+                , MediaObject.content []
+                    [ div [ Attribute.content, style contentStyle ]
+                        [ p []
+                            [ text message.title ]
+                        ]
+                    ]
+                ]
+    in
+        div []
+            (List.map render model.messages)
