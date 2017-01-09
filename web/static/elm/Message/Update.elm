66a17f7df856dd1bec717d2d4763315fb75df0ae
Shodai Yokoyama <quantumcars@gmail.com>
2017-01-09 20:20:18 +0900

    Message: Update - Fetch Message

--- web/static/elm/Message/Update.elm
+++ web/static/elm/Message/Update.elm
@@ -1,12 +1,14 @@
 module Message.Update exposing (..)
 
+import Commands as Commands exposing (..)
 import Message as App exposing (Msg(..))
-import Message.Model exposing (..)
+import Message.Model as Message exposing (..)
 import Message.Msg as Message exposing (..)
-import Models.Message as Message exposing (Message, initialModel)
+import Models as App exposing (..)
+import Models.Message as MessageModel exposing (Message, initialModel)
 
 
-update : App.Msg -> Model -> Model
+update : App.Msg -> Message.Model -> Message.Model
 update message model =
     case message of
         Message msg ->
@@ -16,7 +18,7 @@ update message model =
             model
 
 
-updateModel : Message.Msg -> Model -> Model
+updateModel : Message.Msg -> Message.Model -> Message.Model
 updateModel message model =
     case message of
         Edit _ ->
@@ -25,7 +27,7 @@ updateModel message model =
         New creator ->
             let
                 message =
-                    Message.initialModel
+                    MessageModel.initialModel
                         |> (\m -> { m | creator = creator })
             in
                 { model | message = message, isEditing = True }
@@ -33,19 +35,32 @@ updateModel message model =
         Show _ ->
             { model | isEditing = False }
 
+        Fetch (Ok message) ->
+            { model | message = message }
+
         _ ->
             model
 
 
-updateCommand : App.Msg -> Model -> Cmd App.Msg
-updateCommand message model =
+updateCommand : App.Msg -> App.Model -> Cmd App.Msg
+updateCommand msg { app } =
     Cmd.batch <|
-        case message of
+        case msg of
             Message (Show id) ->
-                []
+                [ fetch app.messages_url id ]
 
             Message (Edit id) ->
-                []
+                [ fetch app.messages_url id ]
 
             _ ->
                 []
+
+
+
+-- COMMAND
+
+
+fetch : Url -> Int -> Cmd App.Msg
+fetch url id =
+    Commands.fetchMessage (url ++ "/" ++ toString id) Fetch
+        |> Cmd.map App.Message
