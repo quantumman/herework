809f02ba8756e5e4102ad80cb483342cf78511fa
Shodai Yokoyama <quantumcars@gmail.com>
2017-01-09 19:06:29 +0900

    Message: Update - update Model

--- web/static/elm/Message/Update.elm
+++ web/static/elm/Message/Update.elm
@@ -3,6 +3,7 @@ module Message.Update exposing (..)
 import Message as App exposing (Msg(..))
 import Message.Model exposing (..)
 import Message.Msg as Message exposing (..)
+import Models.Message as Message exposing (Message, initialModel)
 
 
 update : App.Msg -> Model -> Model
@@ -18,6 +19,14 @@ update message model =
 updateModel : Message.Msg -> Model -> Model
 updateModel message model =
     case message of
+        New creator ->
+            let
+                message =
+                    Message.initialModel
+                        |> (\m -> { m | creator = creator })
+            in
+                { model | message = message }
+
         _ ->
             model
 
@@ -26,7 +35,10 @@ updateCommand : App.Msg -> Model -> Cmd App.Msg
 updateCommand message model =
     Cmd.batch <|
         case message of
-            Message _ ->
+            Message (Show id) ->
+                []
+
+            Message (Edit id) ->
                 []
 
             _ ->
