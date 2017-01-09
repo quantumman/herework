3cf514b5a7f1dd9aaaadb92c65c64031cd9a919e
Shodai Yokoyama <quantumcars@gmail.com>
2017-01-09 21:18:47 +0900

    CommentList: View - Render comment list

--- web/static/elm/CommentList/View/List.elm
+++ web/static/elm/CommentList/View/List.elm
@@ -1,12 +1,60 @@
 module CommentList.View.List exposing (..)
 
 import CommentList.Model as CommentList exposing (..)
+import Component.Infrastructures.DateTime as DateTime exposing (Model, view)
+import Component.UI.Attribute as Attribute exposing (..)
+import Component.UI.MediaObject as MediaObject exposing (..)
+import Date as Date exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Message as App exposing (Msg)
 
 
-view : Model -> Html App.Msg
-view model =
-    div [] []
+-- STYLE
+
+
+avatarStyle : List ( String, String )
+avatarStyle =
+    [ ( "border-radius", "50%" )
+    , ( "width", "32px" )
+    , ( "height", "32px" )
+    ]
+
+
+
+-- VIEW
+
+
+view : DateTime.Model -> CommentList.Model -> Html App.Msg
+view now model =
+    let
+        render comment =
+            MediaObject.media []
+                [ MediaObject.left
+                    [ p [ Attribute.image, class "is-32x32" ]
+                        [ img [ style avatarStyle, src comment.creator.avatar ] [] ]
+                    ]
+                , MediaObject.content []
+                    [ div [ Attribute.content ]
+                        [ p []
+                            [ small []
+                                [ strong []
+                                    [ text comment.creator.name ]
+                                ]
+                            , text " "
+                            , small []
+                                [ createdAt comment.created_at now ]
+                            ]
+                        , text comment.body
+                        ]
+                    ]
+                ]
+    in
+        div []
+            (List.map render model.comments)
+
+
+createdAt : Date -> DateTime.Model -> Html Msg
+createdAt date model =
+    Html.map App.Now <| DateTime.view date model
